25ba76fe9f95a845afb1b079d7b9213ebbf547bc
Shazza-Works <ogormanad@googlemail.com>
2020-05-27 20:46:40 +0100

    Changed_On_Wed May 27 20:46:40 BST 2020

--- color/done_files/simple-test-ps1.sh
+++
@@ -1,4 +0,0 @@
-while [ 1=1 ];do
-	read -p "Enter Your Msg; " msg
-	termux-clipboard-set "[c][00ff00]┏[ff00ff](u0_a238@23.229.155.162)-($(date))[00ff00]┗━>[00ffff](~)[badd11]😆>>>[ffffff][/c][b][i]$msg"
-done
